f058580d090696ceed00e38d7dc8f5f255578652
developerSWe4b <115111381+developerSWe4b@users.noreply.github.com>
2023-02-05 02:42:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const qrcode = require('qrcode-terminal');
+const { MessageMedia } = require('whatsapp-web.js');
+
+const { Client , LocalAuth} = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+    if(message.body === '!verify') {
+const media = await MessageMedia.fromUrl('https://miro.medium.com/max/1400/1*4NhFKMxr-qXodjYpxtiE0w.gif');chat.sendMessage(media);
+		message.reply('You are verifed now \n for send a message you need to type :- !send');
+	}
+    if(message
+    
+message.reply(' Or Hey there you must verify your are a human . \n for verification you need type !verify');
+});
+
+
+client.initialize();
+ 
---
+++ install.sh
@@ -0,0 +1,17 @@
+
+
+echo Installing a Packages
+sleep 1
+echo .
+sleep 1
+echo #############
+echo $ npm i whatsapp-web.js
+sleep 1
+npm i whatsapp-web.js
+sleep 1
+echo $ npm i qrcode-terminal
+sleep 0.4
+npm i qrcode-terminal
+echo "setup ready . "
+
+
